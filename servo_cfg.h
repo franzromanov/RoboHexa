b4d7407fd268d0ab3ac8e310a80ba5c2f9e5a34a
andikaskaban <73022539+andikaskaban@users.noreply.github.com>
2023-05-12 23:29:28 +0800

    Update servo_cfg.h

--- servo_cfg.h
+++ servo_cfg.h
@@ -7,9 +7,10 @@
 #define SWING_TIBIA   16
 #define DUR           300
 
-const unsigned int offsetServoCoxa[6]={120,90,90,120,90,90}; //zero offset servo C,B,F,D,A,E (coxa C rusak)
-const unsigned int offsetServoFemur[6]={75,90,85,90,90,85};   //zero offset servo C,B,F,D,A,E (femur C rusak)
+const unsigned int offsetServoCoxa[6]={90,90,90,120,90,90}; //zero offset servo C,B,F,D,A,E (coxa C rusak)
+const unsigned int offsetServoFemur[6]={70,30,85,85,90,85};   //zero offset servo C,B,F,D,A,E (femur C rusak)
 const unsigned int offsetServoTibia[6]={55,90,80,10,0,80};   //zero offset servo C,B,F,D,A,E
+
 
 int deg_servo(unsigned int* offset,unsigned int legNum);              //offset+sudut servo
 void init_servo();                                                    //fungsi inisialisasi pin servo
@@ -20,3 +21,10 @@ void putar_kanan();                                                   //fungsi p
 void putar_kiri();                                                    //fungsi putar kiri
 void geser_kanan();                                                   //fungsi geser kanan
 void geser_kiri();                                                    //fungsi geser kiri
+void maju_1();
+void kiri_1();
+void maju_2();
+void kiri_2();
+void maju_3();
+void init_gripper();
+void gripper_move();
