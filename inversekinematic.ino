5e031bc1fd38cf3cceadd8fef23b314d05b38b22
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-05-17 20:30:41 +0800

    Update inversekinematic.ino

--- inversekinematic.ino
+++ inversekinematic.ino
@@ -1,12 +1,14 @@
 #include <math.h>
-#include <Servo.h>
+//#include <Servo.h>
 
 #define _T 1000
-#define dlt_t 100
-#define ax 10
-#define ay 10
-#define az 10
-#define a0 10
+#define dlt_t 10
+#define ax 90
+#define ay 3
+#define az 20
+#define a0 60
+#define setoffset -50 
+
 
 float l_z1A,l_z1B;
 float l_y1A,l_y1B;
@@ -16,7 +18,6 @@ float l_y0A,l_y0B;
 float z0=0;
 float theta0,theta1,theta2;
 
-float var_a1,var_a2;
 
 unsigned long current_mill=0;
 unsigned long prev_mill=0;
@@ -54,28 +55,28 @@ float func_y0(float t,float T, float inp_a0){
 
 
 //leg length
-float leg_l(float t,float T, float inp_ax,float inp_a0,float input_az,float inp_z0){
-	return sqrt(pow(inp_ax-func_x0(t,T,inp_ax,inp_a0),2)+pow(func_z1( T, t, inp_az)-inp_z0,2));
+float leg_l(float t,float T, float inp_ax,float inp_a0,float inp_az,float inp_z0){
+  return sqrt(pow(inp_ax-func_x0(t,T,inp_ax,inp_a0),2)+pow(func_z1( T, t, inp_az)-inp_z0,2));
 
 }
-float _d=leg_l(t,T,ax,a0,az,z0);
+float _d=leg_l(_t,_T,ax,a0,az,z0);
 float _a1,_a2;
 
 
 //angle
 
-float func_angle0(float t,float T, float inp_ax,inp_Ay){
-	return atan(func_y1(t,T,inp_Ay)/inp_ax);
+float func_angle0(float t,float T, float inp_ax,float inp_Ay){
+  return atan(func_y1(t,T,inp_Ay)/inp_ax);
 }
 
-float func_angle1(float t,float T,float d float a1, float a2,float inp_ax,float inp_a0){
+float func_angle1(float t,float T,float d ,float a1, float a2,float inp_ax,float inp_a0,float inp_az){
 
-	return atan(func_z1(t,T,inp_az)/(inp_ax-func_x0(t,T,inp_ax,inp_a0))) + acos((pow(a2,2)-pow(a1,2)+pow(d,2))/(2*a1*a2));
+  return atan(func_z1(t,T,inp_az)/(inp_ax-func_x0(t,T,inp_ax,inp_a0))) + acos((pow(a2,2)-pow(a1,2)+pow(d,2))/(2*a1*a2));
 }
 
-float func_angle2(float t,float a1,float a2){
-	return asin((pow(d,2)-(pow(a1,2)+pow(a2,2)))*2*a1*a2);
-	}
+float func_angle2(float t,float a1,float a2,float d){
+  return asin((pow(d,2)-(pow(a1,2)+pow(a2,2)))*2*a1*a2);
+  }
 
 void setup(){
   Serial.begin(9600);
@@ -110,40 +111,42 @@ if(current_mill- prev_mill>=_t){
 
 //angle
 theta0=deg(func_angle0(_t,_T,ax,ay));
-theta1=deg(func_angle1(_t,_T,_d,_a1,_a2,ax,a0));
-theta2=deg(func_angle2(_t,_T,_a1,_a2));
+theta1=deg(func_angle1(_t,_T,_d,_a1,_a2,ax,a0,az));
+theta2=deg(func_angle2(_t,_a1,_a2,_d));
 
 
 //printout
-  Serial.print("l_y1A:");
+  Serial.print("l_y1A=");
   Serial.print(l_y1A);
 
-  Serial.print(",l_y1B:");
+  Serial.print(",l_y1B=");
   Serial.print(l_y1B);
 
-  Serial.print(",l_z1A:");
+  Serial.print(",l_z1A=");
   Serial.print(l_z1A);
 
-  Serial.print(",l_z0A:");
+  Serial.print(",l_z0A=");
   Serial.print(l_z0A);
 
-  Serial.print(",l_z0B:");
+  Serial.print(",l_z0B=");
   Serial.print(l_z0B);
 
-  Serial.print(",l_x0A:");
+  Serial.print(",l_x0A=");
   Serial.println(l_x0A);
 
-  Serial.print("l_x0B:");
+  Serial.print("l_x0B=");
   Serial.println(l_x0B);
 
-  Serial.print("theta0");
+  Serial.print("theta0=");
   Serial.println(theta0);
 
-  Serial.print("theta1");
+  Serial.print("theta1=");
   Serial.println(theta1);
 
-  Serial.print("theta2");
+  Serial.print("theta2=");
   Serial.println(theta2);
 
   Serial.println("");
-}
+
+  delay(120);
+  }
